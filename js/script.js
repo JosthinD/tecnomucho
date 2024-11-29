2360fdc08def8f2fe771adf58d25fbd4e3bc7872
JosthinD <121624470+JosthinD@users.noreply.github.com>
2024-11-28 20:44:27 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// JavaScript para funcionalidades futuras
+console.log("Página cargada correctamente.");
+
+// Agregar un event listener para escuchar el clic
+Buttoninicio.addEventListener('click', () => {
+    // Redireccionar a soportec.html
+  window.location.href = 'index.html';
+});
+
+// Agregar un event listener para escuchar el clic
+Buttonportatiles.addEventListener('click', () => {
+    // Redireccionar a soportec.html
+  window.location.href = 'pagina_portatiles.html';
+});
+
+// Agregar un event listener para escuchar el clic
+Buttondesktops.addEventListener('click', () => {
+    // Redireccionar a soportec.html
+  window.location.href = 'pagina_desktops.html';
+});
+
+// Agregar un event listener para escuchar el clic
+Buttonmonitores.addEventListener('click', () => {
+    // Redireccionar a soportec.html
+  window.location.href = 'pagina_monitores.html';
+});
+
+// Agregar un event listener para escuchar el clic
+Buttonaccesorios.addEventListener('click', () => {
+    // Redireccionar a soportec.html
+  window.location.href = 'pagina_accesorios.html';
+});
